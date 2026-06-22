ced449cfe4be6db4f081ea592788e75a7de7f63c
joaubaron <joaubaron@users.noreply.github.com>
2026-06-22 14:08:22 +0000

    deploy: 732e40f046156faea4ba78c241a3f05159059d8f

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '22.06.2026-1033';
+const CACHE_VERSION = '22.06.2026-1108';
 const CACHE_STATIC  = `equilibrio-static-${CACHE_VERSION}`;
 const CACHE_MP3     = `equilibrio-mp3-${CACHE_VERSION}`;
 const BASE = self.location.pathname.replace('/sw.js', '');
